dd2a16561c2dde7dddb13d4cf95c3297121cdea1
shayveetake2-lang <shayveetake2@gmail.com>
2026-09-26 09:49:01 +1200

    chore: remove test scripts

--- test_login.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_SERVER['REQUEST_METHOD'] = 'POST';
-$_POST = ['username' => 'testuser99', 'password' => 'password123456'];
-require 'api/auth/login.php';
--- test_reg.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_SERVER['REQUEST_METHOD'] = 'POST';
-$_POST = ['username' => 'testuser99', 'email' => 'test99@example.com', 'password' => 'password123456'];
-require 'api/auth/register.php';
